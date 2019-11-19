a5193506961a86cc953c8ea751af8b7b155ea76e
Colm McSharry <colmmcsharry@hotmail.com>
2019-11-18 17:37:00 -0800

    Updates

---
+++ precache-manifest.0b4e63f485ea5aeece93163744878cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caaf6bab601ba48a42adce76492e1637",
+    "url": "/WorkerSandbox/index.html"
+  },
+  {
+    "revision": "4312d6ca2f039b32af3f",
+    "url": "/WorkerSandbox/static/css/2.607d26f6.chunk.css"
+  },
+  {
+    "revision": "71d27467de2af65109a3",
+    "url": "/WorkerSandbox/static/css/main.5fd4ec31.chunk.css"
+  },
+  {
+    "revision": "4312d6ca2f039b32af3f",
+    "url": "/WorkerSandbox/static/js/2.6f58f903.chunk.js"
+  },
+  {
+    "revision": "71d27467de2af65109a3",
+    "url": "/WorkerSandbox/static/js/main.38aa6475.chunk.js"
+  },
+  {
+    "revision": "49b4959382641876921b",
+    "url": "/WorkerSandbox/static/js/runtime~main.8fe307f8.js"
+  },
+  {
+    "revision": "30fc6ca6ea99b8712e1f8064258e694a",
+    "url": "/WorkerSandbox/static/media/networking.30fc6ca6.png"
+  }
+]);
